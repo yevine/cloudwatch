2a0ca34b790b0a578bbb3abefba5879493fcecac
yevine <166761864+yevine@users.noreply.github.com>
2024-06-11 21:05:25 -0400

    Create shellcmd.sh

---
+++ shellcmd.sh
@@ -0,0 +1,14 @@
+  python3
+  python3 spi.py 
+curl -O https://bootstrap.pypa.io/get-pip.py
+ python3 get-pip.py --user
+ pip --version
+pip install Flask
+sudo yum install -y python3-pip python3 python3-setuptools
+pip3 install boto3 -- user
+ pip3 install boto3 --user
+ vim spike.py
+python3 spike.py 
+ top
+ rm -f s*
+ rm -f cpuspike.py 
